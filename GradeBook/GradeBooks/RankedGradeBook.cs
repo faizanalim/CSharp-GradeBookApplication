24c80407e273974b36c3e92b12c22d2ad661138e
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2023-04-29 01:14:07 +0500

    GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,12 +7,39 @@ using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook:BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name):base(name) 
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
-        
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (averageGrade < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            if (averageGrade <= 20)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= 21 && averageGrade <= 40)
+            { 
+                return 'B'; 
+            
+            }
+            else if (averageGrade >= 41 && averageGrade <= 60)
+            {
+                return 'C';
+
+            }
+
+            else if (averageGrade >= 61 && averageGrade <= 80)
+            {
+                return 'D';
+
+            }
+            return 'F';
+        }
     }
 }
